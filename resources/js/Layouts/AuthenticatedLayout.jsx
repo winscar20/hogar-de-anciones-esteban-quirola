dfe4dca86036cf6a5f22b4f0f33468fd05900d3f
Winston Morocho <winstonandrsmp@gmail.com>
2024-11-30 20:42:40 -0500

    Incluir a Jonathan Romero

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -199,7 +199,8 @@ export default function Authenticated({ header, children }) {
             <footer>
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     <div className="text-center text-sm text-gray-600">
-                        &copy; 2024. All rights reserved. Winston Morocho
+                        &copy; 2024. All rights reserved. Winston Morocho &
+                        Jonathan Romero
                     </div>
                 </div>
             </footer>
