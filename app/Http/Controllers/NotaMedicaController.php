e0588bde6cee4089591fe2fcf0c6aff83edc0982
Winston Morocho <winstonandrsmp@gmail.com>
2024-12-04 00:50:19 -0500

    notaMedicaController, funcionalidad

--- app/Http/Controllers/NotaMedicaController.php
+++ app/Http/Controllers/NotaMedicaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\NotaMedica;
+use Carbon\Carbon;
 
 class NotaMedicaController extends Controller
 {
@@ -18,7 +19,7 @@ class NotaMedicaController extends Controller
                     ->orWhere('apellidos', 'LIKE', "%$search%")
                     ->orWhere('cedula', 'LIKE', "%$search%");
             })
-            ->orderBy('fecha', 'desc')
+            ->orderBy('updated_at', 'desc')
             ->paginate(10);
 
         return Inertia::render('NotasMedicas/Index', [
@@ -29,44 +30,72 @@ class NotaMedicaController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return Inertia::render('NotasMedicas/Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+
+        $validate = $request->validate([
+            'paciente' => 'required|exists:pacientes,id',
+            'fecha' => 'required|date',
+            'nota_evaluacion' => 'required|string',
+            'prescripcion_medica' => 'required|string',
+            'user' => 'required|numeric',
+            'redirect' => 'nullable|string',
+        ]);
+        $validate['fecha'] = Carbon::parse($validate['fecha'])->format('Y-m-d H:i:s');
+
+        NotaMedica::create([
+            'id_paciente' => $validate['paciente'],
+            'fecha' => $validate['fecha'],
+            'nota_evaluacion' => $validate['nota_evaluacion'],
+            'prescripcion_medica' => $validate['prescripcion_medica'],
+            'id_user' => $validate['user'],
+        ]);
+        
+        if (array_key_exists('redirect', $validate) && $validate['redirect']) {
+            return redirect()->route($validate['redirect'])->with('success', 'Nota Médica creada correctamente');
+        }
+        return redirect()->route('notas-medicas.index')->with('success', 'Nota Médica creada correctamente');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        //
+        $nota_medica = NotaMedica::with('paciente')->with('user')->findOrFail($id);
+        $nota_medica->fecha = $nota_medica->fecha
+        ? $nota_medica->fecha->format('Y-m-d\TH:i:s')
+        : null;
+
+        return Inertia::render('NotasMedicas/Edit', [
+            'nota' => $nota_medica
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $nota = NotaMedica::findOrFail($id);
+
+        $validate = $request->validate([
+            'paciente' => 'required|exists:pacientes,id',
+            'fecha' => 'required|date',
+            'nota_evaluacion' => 'required|string',
+            'prescripcion_medica' => 'required|string',
+            'user' => 'required|numeric',
+        ]);
+        $validate['fecha'] = Carbon::parse($validate['fecha'])->format('Y-m-d H:i:s');
+
+        $nota->update([
+            'id_paciente' => $validate['paciente'],
+            'fecha' => $validate['fecha'],
+            'nota_evaluacion' => $validate['nota_evaluacion'],
+            'prescripcion_medica' => $validate['prescripcion_medica'],
+            'id_user' => $validate['user'],
+        ]);
+
+        return redirect()->route('notas-medicas.index')->with('success', 'Nota Médica actualizada correctamente');
     }
 
     /**
@@ -74,6 +103,8 @@ class NotaMedicaController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $nota = NotaMedica::findOrFail($id);
+        $nota->delete();
+        return redirect()->route('notas-medicas.index')->with('success', 'Nota Médica eliminada!');
     }
 }
