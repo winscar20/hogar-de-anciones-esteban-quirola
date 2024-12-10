bd61a5dcebec8634ac216330e38d43a4b80d0870
Winston Morocho <winstonandrsmp@gmail.com>
2024-12-10 03:14:09 -0500

    Poner como opcional

--- resources/js/Pages/Pacientes/Edit.jsx
+++ resources/js/Pages/Pacientes/Edit.jsx
@@ -389,7 +389,7 @@ const Edit = ({ paciente }) => {
                             <div className="col-span-2 mt-6 mb-6">
                                 <InputLabel className="block text-gray-700">
                                     Describa las cosas con las que llego el
-                                    Residente:
+                                    Residente (Opcional):
                                 </InputLabel>
                                 <Editor
                                     apiKey="4e42yssu698u0uy1i5qx6ah0gx02y6hsi9nn34ljz0joxuxa"
