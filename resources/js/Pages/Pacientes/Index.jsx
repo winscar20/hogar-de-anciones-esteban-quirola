d53993a835a0cc076c6be5b93abf335a027e418d
Winston Morocho <winstonandrsmp@gmail.com>
2024-12-10 01:04:54 -0500

    Eliminar paciente con busqueda debe blanquear el search despues de eliminar

--- resources/js/Pages/Pacientes/Index.jsx
+++ resources/js/Pages/Pacientes/Index.jsx
@@ -56,6 +56,7 @@ const Index = () => {
                 draggable: true,
                 progress: undefined,
             });
+            setSearchQuery("");
         }
 
         if (flash?.error) {
